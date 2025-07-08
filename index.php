468b5219670ed3f30ddef9a9751c35e5bafe9616
misawa-shota <kaiyou0120@gmail.com>
2025-07-08 23:59:30 +0900

    phpの学習

--- index.php
+++ index.php
@@ -7,14 +7,24 @@
 </head>
 <body>
   <?php
-    $rank = '南';
-    $result = match($rank) {
-      '甲' => '大変良いです。',
-      '乙' => '良いです。',
-      '丙' => 'もう少し頑張りましょう。',
-      default => '???'
+    $score = 90;
+    switch ($score) {
+      case $score >= 90:
+        print '優';
+        break;
+      
+      case $score >= 70:
+        print '良';
+        break;
+      
+      case $score >= 50:
+        print '可';
+        break;
+
+      case 50 > $score:
+        print '不可';
+        break;
     };
-    print $result;
   ?>
 </body>
 </html>
